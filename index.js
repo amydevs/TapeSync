ef064afafbd95dad18f1988bfa9fdf7aca381be1
Amy Y <ctrlalternativedelete@gmail.com>
2020-07-12 01:23:02 +1000

    added extraction

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const asar = require('asar');
 const path = require('path');
+const fs = require('fs');
+
+const src = (path.join(require('os').homedir(), "AppData", "Local", "Programs", "Tape", "resources", "app.asar"));
+const dest = (path.join(require('os').homedir(), "AppData", "Local", "Programs", "Tape", "resources", "app"));
  
-const src = (path.joi);
-const dest = 'name.asar';
- 
-await asar.extractAll(src, dest);
-console.log('done.');
+extract().then(e => {
+    
+})
+
+async function extract() {
+    await asar.extractAll(src, dest);
+    console.log('done.');
+    return;
+}
