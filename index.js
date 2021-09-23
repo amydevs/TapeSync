c1430e6246063c54288d8c91dfd325fe06e8fa13
Amy Y <ctrlalternativedelete@gmail.com>
2021-09-23 17:56:58 +1000

    simplified everything

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-var path = require('path');
+var fs = require("fs-extra");
+var path = require('path')
 var readline = require('readline');
 
-var src = (path.join(require('os').homedir(), "AppData", "Local", "Programs", "Tape", "resources", "app.asar"));
+var src = "./app"
 var dest = (path.join(require('os').homedir(), "AppData", "Local", "Programs", "Tape", "resources", "app"));
-
-const INSTALL = require('./js/install');
-const UNINSTALL = require('./js/uninstall');
-var install = new INSTALL();
-var uninstall = new UNINSTALL();
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+console.log(dest)
 rl.question('Type 1 for INSTALL, and 2 for UNINSTALL: ', (answer) => { 
     rl.close();
-    if (answer == "1") install.installPrompt(src, dest)
-    else if (answer == "2") uninstall.uninstallPrompt(src, dest)
+    if (answer == "1") {
+        fs.copySync(src, dest);
+    }
+    else if (answer == "2") 
+    {
+        fs.removeSync(dest);
+    }
 });
 
